509731dee72af9a5dc47513b1f95ca1dfab0a9fa
Lucas Anderson <lucasanderson351@gmail.com>
2021-12-30 01:36:18 -0300

    Create MergulharBaleia.java

---
+++ Strategy/MergulharBaleia.java
@@ -0,0 +1,8 @@
+public class MergulharBaleia implements MergulharStrategy {
+  
+    @Override
+    public void mergulhar(){
+        System.out.println( "pode preender a respiração por até duas horas" );
+    }
+
+}
