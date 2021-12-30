9b4b632500ffa4446309ac4d663d09fcd8aa3ae2
Joao Pedro Maciel <43148698+JoaoPedroMaciel@users.noreply.github.com>
2021-12-30 01:28:02 -0300

    Create Main.java

---
+++ Strategy/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    Marinho tutubarao = new Tubarao();
+    Marinho cleber = new Golfinho();
+    Marinho perola = new Baleia();
+    Marinho robson = new Baiacu();
+    
+    tutubarao.performarMorder();
+    cleber.performarMergulho();
+    perola.performarNadar();
+    robson.performarTamanho();
+
+  }
+}
