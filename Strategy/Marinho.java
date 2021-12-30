b6c362e7a0116e65fd9eba741d8f67dca0aa78f3
Joao Pedro Maciel <43148698+JoaoPedroMaciel@users.noreply.github.com>
2021-12-30 01:27:04 -0300

    Create Marinho.java

---
+++ Strategy/Marinho.java
@@ -0,0 +1,38 @@
+abstract public class Marinho {
+
+  protected MergulharStrategy mergulhoEstrategico;
+  protected MorderStrategy morderEstrategico;
+  protected TamanhoStrategy tamanhoEstrategico;
+  protected NadarStrategy nadarEstrategico;
+  
+    abstract public void mergulhar();
+    abstract public void morder();
+    abstract public void tamanho();
+    abstract public void nadar();
+
+  public void setMergulharStrategy( MergulharStrategy mergulhoEstrategico ){
+        this.mergulhoEstrategico = mergulhoEstrategico;
+    }
+  public void performarMergulho(){
+        mergulhoEstrategico.mergulhar();
+    }
+
+  public void setMorderStrategy( MorderStrategy morderEstrategico ){
+        this.morderEstrategico = morderEstrategico;
+    }
+  public void performarMorder(){
+        morderEstrategico.morder();
+    }
+  public void setTamanhoStrategy ( TamanhoStrategy tamanhoEstrategico ){
+        this.tamanhoEstrategico = tamanhoEstrategico;
+    }
+  public void performarTamanho(){
+        tamanhoEstrategico.tamanho();
+    }
+  public void setNadarStrategy( NadarStrategy nadarEstrategico ){
+        this.nadarEstrategico = nadarEstrategico;
+    }
+  public void performarNadar(){
+        nadarEstrategico.nadar();
+    }
+}
