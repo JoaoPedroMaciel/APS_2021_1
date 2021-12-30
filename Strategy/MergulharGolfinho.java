0657f5bd8787272d616fa0358bddde34b58eae96
Duda <35925149+eduardaottoni@users.noreply.github.com>
2021-12-30 01:44:53 -0300

    Create MergulharGolfinho.java

---
+++ Strategy/MergulharGolfinho.java
@@ -0,0 +1,8 @@
+public class MergulharGolfinho implements MergulharStrategy {
+  
+    @Override
+    public void mergulhar(){
+        System.out.println( "fica na surfice e brinca um antes de mergulhar" );
+    }
+
+}
